7dc09a6378927c490789deef338f67cc570e837f
John Wiggins <lilwiggy123@gmail.com>
2020-04-26 15:55:50 -0500

    Add spacehuhn command
    Mean to put this in last commit oops

---
+++ src/commands/spacehuhn.ts
@@ -0,0 +1,21 @@
+import { Context } from 'detritus-client/lib/command';
+
+export const spacehuhn = {
+  name: 'spacehuhn',
+  aliases: ['chicken', 'stef'],
+  run: async (ctx: Context) => {
+    if (!ctx.channel?.canEmbedLinks) {
+      ctx.reply('I cannot send embeds in this chat.');
+      return;
+    }
+    let hi = Math.floor(Math.random() * 42);
+    let embed = {
+      title: "Here's your official Spacehuhn meme:tm:",
+      image: { url: `https://spacehuhn.io/img/gallery/${hi}.jpg` },
+      color: 45311,
+    };
+    ctx.reply({
+      embed: embed,
+    });
+  },
+};
