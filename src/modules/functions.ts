9a4cd1616cbe7d4440d8ac592391ba3f37c457b6
John Wiggins <lilwiggy123@gmail.com>
2020-05-01 11:44:22 -0500

    Add level system + comments in functions.ts
    No comments in such a big file was giving me anxiety.

--- src/modules/functions.ts
+++ src/modules/functions.ts
@@ -10,23 +10,24 @@ import { EventHandler } from './utils';
 
 const chanReg = /<#(\d+)>/;
 
+// Additional properties/functions to access on the commandClient
 declare module 'detritus-client/lib/commandclient' {
   interface CommandClient {
-    query: (query: string) => Promise<any>;
-    fetchGuildMember: (ctx: Context) => Member | User | undefined;
-    checkImage: (image: string) => Promise<string>;
-    checkGuild: (id: string, callback: Function) => Promise<void>;
+    query: (query: string) => Promise<any>; // Promise based queries
+    fetchGuildMember: (ctx: Context) => Member | User | undefined; // Easier method to fetch guild members
+    checkImage: (image: string) => Promise<string>; // Checks if an image returns OK before sending
+    checkGuild: (id: string) => Promise<void>; // Checks if a guild is in the database before making SQL calls
+    checkUser: (id: string) => Promise<void>; // Checks if a user is in the database before making SQL calls
     paginate: (
       ctx: Context,
       pages: Array<Page>,
       footer?: string
-    ) => Promise<Paginator>;
-    addEvents: (events: EventHandler[]) => CommandClient;
-    fetchStarData: (message: Message) => Promise<FetchedStarData>;
-
-    job: Job;
-    starQueue: any[];
-    starInterval: Job;
+    ) => Promise<Paginator>; // Handle reaction collectors and pagination
+    addEvents: (events: EventHandler[]) => CommandClient; // Load in events from ./events/
+    fetchStarData: (message: Message) => Promise<FetchedStarData>; // Fetches data for starboard
+    job: Job; // Resets everyone's daily/cookie count at midnight on the server host
+    starQueue: any[]; // A queue of starboard data to process
+    starInterval: Job; // An interval of when to process starboard data
   }
 }
 
@@ -60,6 +61,7 @@ export default (client: CommandClient, connection: Connection) => {
     return m;
   };
 
+  // Is the image OK?
   client.checkImage = async (image: string) => {
     let r = await fetch(image);
     if (r.statusText !== 'OK') return '';
@@ -67,8 +69,22 @@ export default (client: CommandClient, connection: Connection) => {
     return image;
   };
 
+  // Check if user is in the DB before doing anything
+  client.checkUser = async (id: string) => {
+    let result: any[] = await client
+      .query(
+        `SELECT COUNT(*) AS \`count\` FROM \`User\` WHERE \`User_ID\` = ${id}`
+      )
+      .catch(console.error);
+    if (result[0].count === 0) {
+      await client
+        .query(`INSERT INTO \`User\`(\`User_ID\`) VALUES ('${id}')`)
+        .catch(console.error);
+    }
+  };
+
   // Check if the guild is in the DB before doing anything
-  client.checkGuild = async (id: string, callback: Function) => {
+  client.checkGuild = async (id: string) => {
     let result: any[] = await client
       .query(
         `SELECT COUNT(*) AS \`count\` FROM \`Servers\` WHERE \`ServerID\` = '${id}'`
@@ -80,28 +96,40 @@ export default (client: CommandClient, connection: Connection) => {
         .query(`INSERT INTO \`Servers\` (\`ServerID\`) VALUES ('${id}')`)
         .catch(console.error);
     }
-    callback();
   };
 
+  // This handles all the stuff like levels and custom prefixes
   client.onPrefixCheck = async (context: Context) => {
-    if (!context.user.bot && context.guildId) {
+    if (context.user.bot) return '';
+    await client.checkUser(context.user.id);
+    let d = await client
+      .query(
+        `SELECT *, NOW()-INTERVAL 2 MINUTE > \`xp_cool\` AS xpAdd FROM \`User\` WHERE \`User_ID\` = ${context.user.id}`
+      )
+      .catch(console.error);
+    if (d[0].Blacklisted === 1) return '';
+    if (context.guildId) {
       let prefix: string;
       if (context.guild?.prefix) {
         prefix = context.guild.prefix;
       } else {
+        await client.checkGuild(context.guildId);
         let data = await client
           .query(
-            `SELECT \`Prefix\` FROM \`Servers\` WHERE \`ServerID\` = ${context.guildId}`
+            `SELECT \`Prefix\`, \`levels\` FROM \`Servers\` WHERE \`ServerID\` = ${context.guildId}`
           )
           .catch(console.error);
+        context.guild!.levels = data[0].levels;
         prefix = data[0].Prefix;
         context.guild!.prefix = prefix;
       }
+      xpAdd(context, context.guild!.levels, d);
       if (context.message.content.indexOf(prefix) === 0) return prefix;
     }
     return '';
   };
 
+  // This is to help commands with pagination features
   client.paginate = async (
     ctx: Context,
     pages: Array<Page>,
@@ -112,6 +140,7 @@ export default (client: CommandClient, connection: Connection) => {
 
     return p;
   };
+  // This says a command failed and it sends the error message to a chat
   client.on('commandRunError', (err) => {
     err.context.client.channels
       .get('686714427650736133')
@@ -122,11 +151,13 @@ export default (client: CommandClient, connection: Connection) => {
       `An error has occured! This incident and proper context has been reported to my dev team. I apologize for the inconvenience.`
     );
   });
+  // This tells me when someone runs a command. Useful for debugging
   client.on('commandRan', (cmd) => {
     console.log(
       `Ran command ${cmd.command.name} by ${cmd.context.member!.username}`
     );
   });
+  // This runs checks on commands when they are set in place so we don't have to do it for each file
   client.onCommandCheck = async (ctx: Context, command: Command) => {
     if (!command.metadata.checks) {
       return true;
@@ -191,6 +222,7 @@ export default (client: CommandClient, connection: Connection) => {
     return true;
   };
 
+  // This fetches starboard data
   client.fetchStarData = async (message: Message) => {
     let starData: StarData[] = await client.query(
       `SELECT COUNT(*) AS \`count\`, \`msgID\`, \`starID\` FROM \`starboard\` WHERE \`msgID\` = ${message.id} OR \`starID\` = ${message.id}`
@@ -220,6 +252,7 @@ export default (client: CommandClient, connection: Connection) => {
     };
   };
 
+  // This adds commands based on classes
   client.addMultiple = (commands?: CommandOptions[]) => {
     commands?.forEach((command) => {
       client.add(command);
@@ -228,6 +261,7 @@ export default (client: CommandClient, connection: Connection) => {
     return client;
   };
 
+  // This adds events based on classes
   client.addEvents = (events: EventHandler[]) => {
     events.forEach((el) => {
       client.client.addListener(
@@ -240,5 +274,29 @@ export default (client: CommandClient, connection: Connection) => {
     return client;
   };
 
+  // Initiates the starboard queue
   client.starQueue = [];
+  // This is the function that handled adding experience to people. Keeps the prefixCheck clean
+  async function xpAdd(ctx: Context, enabled: number, userData: any[]) {
+    if (
+      userData[0].xpAdd === 1 ||
+      userData[0].xpAdd === null ||
+      userData[0].xpAdd === undefined
+    ) {
+      let xp: number = Math.floor(Math.random() * 50); // 50 xp max at random. Just to make leveling up hard as pee pee
+      await client.query(
+        `UPDATE \`User\` SET \`xp_cool\`=NOW(), \`XP\`=\`XP\` + '${xp}' WHERE \`User_ID\` = ${ctx.user.id}`
+      );
+      if (userData[0].XP > userData[0].Next && enabled === 1) {
+        ctx.reply(
+          `Congrats ${ctx.user.username}! You just leveled up to level ${
+            userData[0].Level + 1
+          }`
+        );
+        await client.query(
+          `UPDATE \`User\` SET \`Level\` = \`Level\` + 1, \`Next\` = \`Next\` + 500, \`xp\` = 0 WHERE \`User_ID\` = ${ctx.user.id}`
+        );
+      }
+    }
+  }
 };
