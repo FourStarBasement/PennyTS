6d4c0b97637c61bd7bc442a3f94cf9b6141d9183
NCPlayz <chowdhurynadir0@outlook.com>
2020-05-06 22:16:11 +0100

    Check for new bots in guildMemberAdd

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,6 +1,9 @@
-import { ClientEvents } from 'detritus-client/lib/constants';
+import { ClientEvents, AuditLogActions } from 'detritus-client/lib/constants';
 import { CommandClient, GatewayClientEvents } from 'detritus-client';
 import { Servers } from '../modules/db';
+import { ModLogActions } from '../modules/modlog';
+import { AuditLog } from 'detritus-client/lib/structures';
+import { RequestTypes } from 'detritus-client-rest/lib/types';
 
 export const guildMemberAdd = {
   event: ClientEvents.GUILD_MEMBER_ADD,
@@ -15,6 +18,29 @@ export const guildMemberAdd = {
       let results: Servers[] = await client.query(
         `SELECT * FROM \`Servers\` WHERE \`ServerID\` = '${payload.guildId}'`
       );
+
+      let channel = guild.channels.get(results[0].mod_channel);
+      if (channel) {
+        if (
+          (ModLogActions.GUILD_MEMBER_ADD & guild.modLog) ===
+          ModLogActions.GUILD_MEMBER_ADD
+        ) {
+          if (member.bot) {
+            guild
+              .fetchAuditLogs({
+                actionType: AuditLogActions.BOT_ADD,
+              })
+              .then((audit) => {
+                channel!.createMessage(
+                  makeEmbed(
+                    audit.find((v, k) => v.targetId === payload.userId)!,
+                    payload
+                  )
+                );
+              });
+          }
+        }
+      }
 
       if (results[0].Welcome === 1) {
         const channel = guild.channels.get(results[0].wc);
@@ -42,3 +68,28 @@ export const guildMemberAdd = {
     });
   },
 };
+
+function makeEmbed(
+  audit: AuditLog,
+  payload: GatewayClientEvents.GuildMemberAdd
+): RequestTypes.CreateMessage {
+  return {
+    embed: {
+      author: {
+        iconUrl: payload.member.avatarUrl,
+        name: `${payload.member.username}#${payload.member.discriminator} (${payload.userId})`,
+      },
+      color: 39219,
+      title: 'Bot Added',
+      fields: [
+        {
+          name: 'Added by',
+          value: `${audit.user!.username}#${audit.user!.discriminator} (${
+            audit.userId
+          })`,
+        },
+      ],
+      timestamp: new Date().toISOString(),
+    },
+  };
+}
