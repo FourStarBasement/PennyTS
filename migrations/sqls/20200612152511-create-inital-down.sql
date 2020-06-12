41a07e2e2c67b18d8a6de4ba7be975b7537db15a
PixeL <officialpixelinc@gmail.com>
2020-06-12 12:04:08 -0500

    Add basic migration, using db-migrate

---
+++ migrations/sqls/20200612152511-create-inital-down.sql
@@ -0,0 +1,9 @@
+/* Replace with your SQL commands */
+DROP TABLE emote;
+DROP TABLE roles;
+DROP TABLE servers;
+DROP TABLE starboard;
+DROP TABLE tags;
+DROP TABLE users;
+DROP TABLE user_backgrounds;
+DROP TABLE user_emblems;
