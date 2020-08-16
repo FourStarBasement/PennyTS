be20dffd60a2c2dc646c7c268509891494792cc5
Lilwiggy <lilwiggy123@gmail.com>
2020-08-15 23:14:16 -0500

    Remove useless check

--- src/commands/owner/blacklist.ts
+++ src/commands/owner/blacklist.ts
@@ -25,16 +25,7 @@ export const blacklist = {
 
     let mentioned = ctx.users.get(blacklistArgs[0]);
 
-    if (!mentioned!.blacklisted) {
-      const user: DBUser = await ctx.commandClient.preparedQuery(
-        'SELECT blacklisted FROM users WHERE user_id = $1',
-        [blacklistArgs[0]],
-        QueryType.Single
-      );
-      mentioned!.blacklisted = user.blacklisted;
-    }
-
-    if (mentioned?.blacklisted) {
+    if (mentioned!.blacklisted) {
       await ctx.commandClient.preparedQuery(
         'UPDATE users SET blacklisted = false WHERE user_id = $1',
         [blacklistArgs[0]],
@@ -47,27 +38,27 @@ export const blacklist = {
       ctx.client.channels
         .get('708062124160581683')
         ?.createMessage(`${mentioned?.username} was unblacklisted.`);
-    } else {
-      let reason = stringExtractor(args.blacklist)[0];
-
-      await ctx.commandClient.preparedQuery(
-        'UPDATE users SET blacklisted = true WHERE user_id = $1',
-        [blacklistArgs[0]],
-        QueryType.Void
-      );
-
-      ctx.reply(`${mentioned} has been blacklisted`);
-      mentioned!.blacklisted = true;
-
-      mentioned?.createMessage(
-        'I have come to inform you that you have been blacklisted from penny. You can find out more here: https://discord.gg/kwcd9dq'
-      );
-
-      ctx.client.channels
-        .get('708062124160581683')
-        ?.createMessage(
-          `${mentioned?.username} was just blacklisted for the following reason:\n${reason}`
-        );
+      return;
     }
+    mentioned!.blacklisted = true;
+    let reason = stringExtractor(args.blacklist)[0];
+
+    await ctx.commandClient.preparedQuery(
+      'UPDATE users SET blacklisted = true WHERE user_id = $1',
+      [blacklistArgs[0]],
+      QueryType.Void
+    );
+
+    ctx.reply(`${mentioned} has been blacklisted`);
+
+    mentioned?.createMessage(
+      'I have come to inform you that you have been blacklisted from penny. You can find out more here: https://discord.gg/kwcd9dq'
+    );
+
+    ctx.client.channels
+      .get('708062124160581683')
+      ?.createMessage(
+        `${mentioned?.username} was just blacklisted for the following reason:\n${reason}`
+      );
   },
 };
