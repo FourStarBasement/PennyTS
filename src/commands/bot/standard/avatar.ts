3a7f8a87b12367972af4ae6d1f3bdd9536d04d4f
Lilwiggy <lilwiggy123@gmail.com>
2021-10-25 01:10:52 -0700

    Support guild specific avatars in avatar command

--- src/commands/bot/standard/avatar.ts
+++ src/commands/bot/standard/avatar.ts
@@ -10,16 +10,21 @@ export const avatar = {
   run: async (ctx: Context) => {
     let user =
       (ctx.commandClient.fetchGuildMember(ctx) as User) || ctx.message.author;
+    console.log(ctx.member);
     if (!user.avgColor)
       user.avgColor = await ctx.commandClient.fetchAverageColor(user.avatarUrl);
-    ctx.reply({
-      embed: {
-        title: `${user.username}'s avatar.`,
-        image: {
-          url: `${user.avatarUrl}?size=2048`,
-        },
-        color: user.avgColor,
+    let embed = {
+      title: `${user.username}'s avatar.`,
+      image: {
+        url: `${user.avatarUrl}?size=2048`,
       },
+      color: 6969,
+    };
+    if (ctx.member && ctx.member.avatar !== user.avatar)
+      embed.image = { url: `${ctx.member.avatarUrl}?size=2048` };
+
+    ctx.reply({
+      embed: embed,
     });
   },
 };
